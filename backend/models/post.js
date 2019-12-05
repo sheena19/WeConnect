dbe4f2b0848c753e848961905fee77fee0759a1a
Sheena Shah <shah.sheena19@gmail.com>
2019-12-04 23:49:21 -0500

    backend changes

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  content: {type: String, required: true },
-  imagePath: {type: String, required: true}
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  imagePath: {type: String, required: true},
+  creator: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true} // giving a reference of which ID it is
 });
 module.exports = mongoose.model('Post', postSchema);
